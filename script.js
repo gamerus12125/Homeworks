23b2e1bbaeab668c4ad7669d094de5bd2f4492ea
gamerus12125 <Mik78456@yandex.ru>
2021-12-11 17:33:03 +0300

    Домашнее задание 11.12.2021

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
 "use strict";
 
 //task 1
-let car = {
-  drive: function (time) {
-    return time * this.speed;
+class Worker {
+  constructor(firstName, lastName, rate, days) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.rate = rate;
+    this.days = days;
+  }
+  getSalary = () => {
+    let salary = this.rate * this.days;
+    console.log(salary);
+  };
+}
+class Developer extends Worker {
+  constructor(firstName, lastName, rate, days, skill, company) {
+    super(firstName, lastName, rate, days);
+    this.skill = skill;
+    this._company = company;
+  }
+  getProfile() {
+    let salary = this.rate * this.days;
+    console.log(this.firstName, this.lastName, this.rate, salary, this.skill);
+  }
+  get company() {
+    console.log(this._company);
+  }
+}
+let salary = new Worker("John", "Doe", "100", "7");
+salary.getSalary();
+let developer = new Developer(
+  "Mike",
+  "Doe",
+  "200",
+  "6",
+  "experienced",
+  "Microsoft"
+);
+developer.getProfile();
+
+//task 2
+
+const props = {
+  user: {
+    login: "Admin",
+    password: "1234",
+  },
+  info: {
+    protocol: "https",
+    host: "123.45.6.7",
+    port: "1234",
   },
 };
-let bmv = {
-  model: "m3",
-  speed: 280,
-};
-bmv.__proto__ = car;
-let toyota = {
-  model: "supra",
-  speed: 260,
-};
-toyota.__proto__ = car;
-let mazda = {
-  model: "mx 5",
-  speed: 220,
-};
-mazda.__proto__ = car;
-console.log(
-  bmv.drive(3),
-  bmv.drive(10),
-  toyota.drive(3),
-  toyota.drive(10),
-  mazda.drive(3),
-  mazda.drive(10)
-);
+
+const { login, password } = props.user;
+const { protocol, host, port } = props.info;
+console.log(login, password, protocol, host, port);
+
+//stask 3
+
+const coordinates = [12, 134, 567, 456, 13, 14];
+let [first, , , fourth, , sixth] = coordinates;
+console.log(first, fourth, sixth);
