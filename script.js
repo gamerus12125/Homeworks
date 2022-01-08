5c7461544f9cfb4e1b221dfde3fc13c5bb8b1f87
gamerus12125 <Mik78456@yandex.ru>
2022-01-08 17:36:25 +0300

    Домашнее задание на 08.01.2022

--- script.js
+++ script.js
@@ -1,64 +1,38 @@
 "use strict";
-
-//task 1
-class Worker {
-  constructor(firstName, lastName, rate, days) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.rate = rate;
-    this.days = days;
-  }
-  getSalary = () => {
-    let salary = this.rate * this.days;
-    console.log(salary);
-  };
+// task 1
+function fetchPromise() {
+  fetch("https://web-app-papatomatoe.herokuapp.com/posts")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  fetch("https://web-app-papatomatoe.herokuapp.com/posts/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  fetch("https://web-app-papatomatoe.herokuapp.com/sections")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
-class Developer extends Worker {
-  constructor(firstName, lastName, rate, days, skill, company) {
-    super(firstName, lastName, rate, days);
-    this.skill = skill;
-    this._company = company;
-  }
-  getProfile() {
-    let salary = this.rate * this.days;
-    console.log(this.firstName, this.lastName, this.rate, salary, this.skill);
-  }
-  get company() {
-    console.log(this._company);
-  }
+async function fetchAsync() {
+  let response = await fetch("https://web-app-papatomatoe.herokuapp.com/posts");
+  let posts = await response.json();
+  console.log(posts);
+  response = await fetch("https://web-app-papatomatoe.herokuapp.com/posts/1");
+  posts = await response.json();
+  console.log(posts);
+  response = await fetch("https://web-app-papatomatoe.herokuapp.com/sections");
+  posts = await response.json();
+  console.log(posts);
 }
-let salary = new Worker("John", "Doe", "100", "7");
-salary.getSalary();
-let developer = new Developer(
-  "Mike",
-  "Doe",
-  "200",
-  "6",
-  "experienced",
-  "Microsoft"
-);
-developer.getProfile();
+fetchPromise();
+fetchAsync();
 
 //task 2
 
-const props = {
-  user: {
-    login: "Admin",
-    password: "1234",
-  },
-  info: {
-    protocol: "https",
-    host: "123.45.6.7",
-    port: "1234",
-  },
-};
-
-const { login, password } = props.user;
-const { protocol, host, port } = props.info;
-console.log(login, password, protocol, host, port);
-
-//stask 3
-
-const coordinates = [12, 134, 567, 456, 13, 14];
-let [first, , , fourth, , sixth] = coordinates;
-console.log(first, fourth, sixth);
+//task 3
+async function saveLocalStorage() {
+  let response = await fetch("https://web-app-papatomatoe.herokuapp.com/posts");
+  let posts = await response.text();
+  localStorage.setItem("Posts", posts);
+}
+saveLocalStorage();
+const Posts = localStorage.getItem("Posts");
+console.log(Posts);
